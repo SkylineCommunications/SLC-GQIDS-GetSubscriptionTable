d5a74688f1020a5fee99585070cfc45eda0cbdb8
SKYLINE2\CarlosMT <carlos.matheus@skyline.be>
2025-06-18 09:42:44 -0400

    quick push for github

--- GetSubscriptionTable_1/GetSubscriptionTable_1.cs
+++ GetSubscriptionTable_1/GetSubscriptionTable_1.cs
@@ -1,88 +1,97 @@
 namespace GetSubscriptionTable_1
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Linq;
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Text.RegularExpressions;
 
-    using Skyline.DataMiner.Analytics.GenericInterface;
-    using Skyline.DataMiner.Automation;
-    using Skyline.DataMiner.Net.Messages;
+	using Skyline.DataMiner.Analytics.GenericInterface;
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Net;
+	using Skyline.DataMiner.Net.Exceptions;
+	using Skyline.DataMiner.Net.Helper;
+	using Skyline.DataMiner.Net.Messages;
 
-    /// <summary>
-    /// Represents a data source.
-    /// See: https://aka.dataminer.services/gqi-external-data-source for a complete example.
-    /// </summary>
-    [GQIMetaData(Name = "GetSubscriptionTable")]
-    public sealed class GetSubscriptionTable : IGQIDataSource, IGQIOnInit, IGQIInputArguments
-    {
-        private readonly GQIStringArgument nameFilter = new GQIStringArgument("Name Filter") { IsRequired = false };
+	/// <summary>
+	/// Represents a data source.
+	/// See: https://aka.dataminer.services/gqi-external-data-source for a complete example.
+	/// </summary>
+	[GQIMetaData(Name = "Get Subscriptions")]
+	public sealed class GetSubscriptionTable : IGQIDataSource, IGQIOnInit, IGQIInputArguments
+	{
+		private readonly GQIStringArgument nameFilter = new GQIStringArgument("Name Filter") { IsRequired = false };
 
-        private string _nameFilter;
-        private GQIDMS _dms;
+		private string _nameFilter;
+		private GQIDMS _dms;
 
-        public OnInitOutputArgs OnInit(OnInitInputArgs args)
-        {
-            _dms = args.DMS;
-            return new OnInitOutputArgs();
-        }
+		public OnInitOutputArgs OnInit(OnInitInputArgs args)
+		{
+			_dms = args.DMS;
+			return new OnInitOutputArgs();
+		}
 
-        public GQIArgument[] GetInputArguments()
-        {
-            return new GQIArgument[] { nameFilter };
-        }
+		public GQIArgument[] GetInputArguments()
+		{
+			return new GQIArgument[] { nameFilter };
+		}
 
-        public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
-        {
-            _nameFilter = args.GetArgumentValue(nameFilter);
-            return new OnArgumentsProcessedOutputArgs();
-        }
+		public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+		{
+			_nameFilter = args.GetArgumentValue(nameFilter);
+			return new OnArgumentsProcessedOutputArgs();
+		}
 
-        public GQIColumn[] GetColumns()
-        {
-            return new GQIColumn[]
-            {
-                    new GQIStringColumn("Subscription Name"),
-                    new GQIDateTimeColumn("Connection Time"),
-            };
-        }
+		public GQIColumn[] GetColumns()
+		{
+			return new GQIColumn[]
+			{
+				new GQIStringColumn("Connection ID"),
+				new GQIStringColumn("Subscription Name"),
+				new GQIDateTimeColumn("Connection Time"),
+			};
+		}
 
-        public GQIPage GetNextPage(GetNextPageInputArgs args)
-        {
-            var rows = GetSubscriptions().ToArray();
-            return new GQIPage(rows)
-            {
-                HasNextPage = false,
-            };
-        }
+		public GQIPage GetNextPage(GetNextPageInputArgs args)
+		{
+			var rows = GetSubscriptions().ToArray();
+			return new GQIPage(rows)
+			{
+				HasNextPage = false,
+			};
+		}
 
-        private IEnumerable<GQIRow> GetSubscriptions()
-        {
-            var rows = new List<GQIRow>();
+		private IEnumerable<GQIRow> GetSubscriptions()
+		{
+			var rows = new List<GQIRow>();
 
-            var clientInfoMessagerResponse = _dms.SendMessages(new GetInfoMessage(InfoType.ClientList));
-            var responses = clientInfoMessagerResponse?.OfType<LoginInfoResponseMessage>().ToList();
-            int i = 0;
-            foreach (var response in responses)
-            {
-                if (!response.FriendlyName.Contains(_nameFilter))
-                {
-                    continue;
-                }
+			var clientInfoMessagerResponse = _dms.SendMessages(new GetInfoMessage(InfoType.ClientList));
+			var responses = clientInfoMessagerResponse?.OfType<LoginInfoResponseMessage>().ToList();
 
-                var cells = new[]
-                {
-                    new GQICell { Value = Convert.ToString(response.FriendlyName )}, // Channel Id
-                    new GQICell { Value = response.ConnectTime.ToUniversalTime() },
-                };
+			foreach (var response in responses)
+			{
+				if (response == null)
+					continue;
 
-                var row = new GQIRow(i.ToString(), cells);
-                i++;
+				if (!_nameFilter.IsNullOrEmpty() && !response.FriendlyName.Contains(_nameFilter))
+				{
+					continue;
+				}
 
-                rows.Add(row);
-            }
+				var cells = new[]
+				{
+					new GQICell { Value = Convert.ToString(response.ConnectionID )},
+					new GQICell { Value = Convert.ToString(response.FriendlyName )},
+					new GQICell { Value = response.ConnectTime.ToUniversalTime() },
+				};
 
-            return rows;
-        }
-    }
+				var row = new GQIRow(Convert.ToString(response.ConnectionID), cells);
+
+				rows.Add(row);
+			}
+
+			return rows;
+		}
+	}
 }
